aef6e936f96d1e24a0480b55d6a4103fac2206cc
Fedeg91 <federico.giuliani05@gmail.com>
2023-12-25 18:56:30 +0100

    last

--- src/App.js
+++ src/App.js
@@ -207,12 +207,12 @@ function App() {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            height: "50%",
+            height: "100%",
             backgroundImage: "url('../public//Siviglia/Siviglia_6.jpg')",
           }}
         >
           <Animator animation={ZoomInScrollOut}>
-            <img src="./andalucia.svg" width={"250px"} />
+            <img src="./andalucia.svg" width={"230px"} />
           </Animator>
         </div>
       </ScrollPage>
@@ -223,7 +223,7 @@ function App() {
             <Animator animation={FadeUp}>
               <span
                 style={{
-                  fontSize: "50px",
+                  fontSize: "35px",
                   display: "flex",
                   aligItems: "center",
                   justifyContent: "center",
@@ -236,48 +236,130 @@ function App() {
           </ScrollPage>
         </div>
       </div>
-      <ScrollPage page={7}>
+      <div>
         <div
           style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            height: "100%",
+            height: "100vh",
+            padding: "2em",
           }}
         >
           <span style={{ fontSize: "30px" }}>
-            <Animator animation={MoveIn(-1000, 0)}>
-              <div
-                style={{
-                  flexDirection: "column",
-                  display: "flex",
-                  justifyContent: "column",
-                  alignItems: "center",
-                  textAlign: "center",
-                }}
-              >
-                Guarda l'itinerario ✈️<span> 💛💛💛</span>
-              </div>
-            </Animator>
+            <div
+              style={{
+                flexDirection: "column",
+                display: "flex",
+                justifyContent: "column",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              Guarda l'itinerario ✈️<span> 💛💛💛</span>
+            </div>
           </span>
         </div>
-      </ScrollPage>
+      </div>
+
       <ScrollPage page={8}>
         <div className="section-alhambra">
-          <h2>
-            <Animator animation={MoveIn(-1000, 0)}>Day 1</Animator>
-            <Animator animation={MoveIn(1000, 0)}>Nice to meet you 🙋🏻‍♀️</Animator>
-            - I'm Dante Chun -
-            <Animator animation={MoveOut(1000, 0)}>Good bye ✋🏻</Animator>
-            <Animator animation={MoveOut(-1000, 0)}>See you 💛</Animator>
-          </h2>
+          <Animator animation={MoveIn(-1000, 0)}>
+            <h3>Day 1</h3>
+          </Animator>
+          <Animator animation={MoveIn(1000, 0)}>
+            <h1>Granada</h1>
+            <p style={{ fontSize: "24px" }}>Visita della città e Albaizin</p>
+          </Animator>
         </div>
       </ScrollPage>
-      <ScrollPage page={4}>
-        <Animator animation={batch(Fade(), Sticky())}>
-          <span style={{ fontSize: "30px" }}>Auguri </span>
-        </Animator>
+      <ScrollPage page={9}>
+        <div className="section-alhambra">
+          <Animator animation={MoveIn(-1000, 0)}>
+            <h2>Day 2</h2>
+          </Animator>
+          <Animator animation={MoveIn(1000, 0)}>
+            <h1>Alhambra</h1>
+            <p style={{ fontSize: "24px" }}>Visita del castello</p>
+          </Animator>
+        </div>
       </ScrollPage>
+      <ScrollPage page={10}>
+        <div className="section-cordoba">
+          <Animator animation={MoveIn(-1000, 0)}>
+            <h3>Day 3</h3>
+          </Animator>
+          <Animator animation={MoveIn(1000, 0)}>
+            <h1>Cordoba</h1>
+            <p style={{ fontSize: "24px" }}>
+              Visita della Mezquita e della juderia
+            </p>
+          </Animator>
+        </div>
+      </ScrollPage>
+      <ScrollPage page={11}>
+        <div className="section-cordoba">
+          <Animator animation={MoveIn(-1000, 0)}>
+            <h2>Day 4</h2>
+          </Animator>
+          <Animator animation={MoveIn(1000, 0)}>
+            <h1>Mezquita</h1>
+            <p style={{ fontSize: "24px" }}>
+              Altro giro in città e partenza per...
+            </p>
+          </Animator>
+        </div>
+      </ScrollPage>
+      <ScrollPage page={10}>
+        <div className="section-sevilla">
+          <Animator animation={MoveIn(-1000, 0)}>
+            <h3>Day 5</h3>
+          </Animator>
+          <Animator animation={MoveIn(1000, 0)}>
+            <h1>Alcazer</h1>
+            <p style={{ fontSize: "24px" }}>
+              Visita della città e dell'Alcazer
+            </p>
+          </Animator>
+        </div>
+      </ScrollPage>
+      <ScrollPage page={11}>
+        <div className="section-sevilla">
+          <Animator animation={MoveIn(-1000, 0)}>
+            <h2>Day 6</h2>
+          </Animator>
+          <Animator animation={MoveIn(1000, 0)}>
+            <h1>Sevilla</h1>
+            <p style={{ fontSize: "24px" }}>giro e città e adios espana</p>
+          </Animator>
+        </div>
+      </ScrollPage>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            fontSize: "30px",
+            fontWeight: 600,
+            textAlign: "center",
+            justifyContent: "center",
+
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            height: "100vh",
+          }}
+        >
+          <span style={{ fontSize: "50px" }}>🎅🏻</span>OH-OH-OH !
+          <button className="button">Scarica il biglietto qui</button>
+        </div>
+      </div>
     </ScrollContainer>
   );
 }
